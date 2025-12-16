db298043aa07c57bdaa17bd88a9f6df1e38b95ca
jujedev <maurimaldonado6@gmail.com>
2025-12-15 22:00:08 -0300

    First push

---
+++ src/main/java/com/proyectofinaltalentotech/app/ProyectoFinalTalentotechMaldonadomauricioApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinaltalentotech.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalTalentotechMaldonadomauricioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalTalentotechMaldonadomauricioApplication.class, args);
+	}
+
+}
